247069d374f84e9a7dc716c3beb255e08cff2183
Mykola Romaniuk <niko_42na@ukrbash.org>
2016-11-22 20:07:47 +0200

    2 commit

--- background.js
+++ background.js
@@ -1,11 +1,16 @@
-let interval = setInterval(() => {
-navigator.getBattery()
- .then((battery) => {
-  let level = battery.level * 100;
-     if (level <= 20 && !battery.charging) {
- alert(‘Battery level is too low. You should connect your leptop to charge device’);
 
-      clearInterval(interval);
-     }
-  })
+let interval = setInterval(() => {
+
+  navigator.getBattery()
+        .then((battery) => {
+
+          let level = battery.level * 100;
+
+          if (level <= 20 && !battery.charging) {
+            alert('Battery level is too low. You should connect your leptop to charge device');
+            clearInterval(interval);
+
+          }
+      })
+
 }, 60000);
--- popup/main.js
+++ popup/main.js
@@ -1,23 +1,27 @@
 const getStyle = (level) => {
-	if (level <= 10) {
- 		return ‘empty’;
-	} else if (level <= 20) {
- 		return ‘low’;
-	} else if (level <= 50) {
- 		return ‘medium’;
- 	} else if (level <= 80) {
- 		return ‘good’;
- 	}
-	return ‘full’
-}
+      if (level <= 10) {
+        return 'empty';
+      }
+      else if (level <= 20) {
+        return 'low';
+      }
+      else if (level <= 50) {
+        return 'medium';
+      }
+      else if (level <= 80) {
+        return 'good';
+      }
 
-document.getElementById(‘batteryBody’).setAttribute(‘class’, getStyle(level))
-document.getElementById(‘indicator’).style.height = 100 — level + ‘%’;
+      return 'full'
+  }
+
 
 navigator.getBattery()
-	.then((battery) => {    
-		let level = battery.level * 100; 
-		document.getElementById(‘status’).innerHTML = `${level.toFixed(0)}%`;
- 		document.getElementById(‘indicator’).style.height = 100 — level + ‘%’;
- 		document.getElementById(‘batteryBody’).setAttribute(‘class’, getStyle(level))
-	})
+    .then((battery) => {
+
+      let level = battery.level * 100;
+
+      document.getElementById('status').innerHTML = `${level.toFixed(0)}%`;
+      document.getElementById('indicator').style.height = 100 - level + '%';
+      document.getElementById('batteryBody').setAttribute('class', getStyle(level))
+    })
